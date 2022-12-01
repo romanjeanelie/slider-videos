d0416ceb1e05019a03b942a6c8b9d44c4d2371d1
Roman Jean-Elie <romanjeanelie@yahoo.fr>
2022-12-01 17:57:41 +0100

    add preload-it

--- src/App.jsx
+++ src/App.jsx
@@ -4,150 +4,57 @@ import AssetsGenerator from "@/modules/AssetsGenerator";
 import { READY_STATES } from "@/constants";
 import { throttle } from "lodash";
 
-const videoLinks = [
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/745429773/rendition/1080p/file.mp4?loc=external&signature=9a1f98f1c56ffd0b67690bb358d1c897a02390b0d2d2b8c841e80478cbc69273",
-    },
-  },
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-    },
-  },
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/745438360/rendition/1080p/file.mp4?loc=external&signature=d9cd1b7eb2e898e30e8abb2764d2e8b9d4ea1ce0f7e78101a5e2998e4d5de042",
-    },
-  },
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/745439503/rendition/1080p/file.mp4?loc=external&signature=73b07aa2a65ce25f9f0533cd207d1ad5907489bf596ebe1d37e7fb2274434855",
-    },
-  },
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/745435539/rendition/1080p/file.mp4?loc=external&signature=006c81c361a16764fb9bd8b6e2a23b6bab008e9dd563eef80e206db62a806f2b",
-    },
-  },
-  {
-    type: "video",
-    video: {
-      url: "https://player.vimeo.com/progressive_redirect/playback/755610395/rendition/1080p/file.mp4?loc=external&signature=9952f9d547936b19357794a86e1d4cacffeefc9665ca1665ac28ecfbe8df55ea",
-    },
-  },
-];
+import Preload from "preload-it";
 
-// const videos = [
-//   {
-//     key: 0,
-//     label: "model-1",
-//     main: "https://player.vimeo.com/progressive_redirect/playback/745429773/rendition/1080p/file.mp4?loc=external&signature=9a1f98f1c56ffd0b67690bb358d1c897a02390b0d2d2b8c841e80478cbc69273",
-//     details: [
-//       {
-//         label: "top",
-//         url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-//       },
-//       {
-//         label: "bottom",
-//         url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-//       },
-//     ],
-//   },
-//   {
-//     key: 1,
-//     label: "model-2",
-//     main: "https://player.vimeo.com/progressive_redirect/playback/745429773/rendition/1080p/file.mp4?loc=external&signature=9a1f98f1c56ffd0b67690bb358d1c897a02390b0d2d2b8c841e80478cbc69273",
-//     details: [
-//       {
-//         label: "top",
-//         url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-//       },
-//       {
-//         label: "bottom",
-//         url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-//       },
-//     ],
-//   },
-// ];
+import styled from "styled-components";
 
-const getAssets = () => {
-  const assets = new Map();
+const videoLinks = ["/videos/1.mp4", "/videos/2.mp4", "/videos/3.mp4", "/videos/4.mp4"];
 
-  controlRoomConfig.map((el, index) => {
-    el.assets.map((asset) => {
-      const { name, type, video, image } = asset;
-      assets.set(
-        {
-          name,
-          screen: el.screen,
-          index,
-        },
-        { type, video, image }
-      );
-    });
-  });
-  return assets;
-};
-
-const THROTTLE_WAIT = 1000 / 60;
+const Loader = styled.div`
+  text-align: center;
+  p {
+    margin: 4px 0;
+  }
+`;
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const [worker, setWorker] = useState(null);
-  const [assets, setAssets] = useState(null);
+  const [videos, setVideos] = useState([]);
 
-  // Refs
-  const refGeneratorWorker = useRef();
+  // Ref
+  const preloadRef = useRef();
+  const progressRef = useRef();
 
-  // Init worker
+  // Preload videos
   useEffect(() => {
-    if (refGeneratorWorker.current) return;
+    if (!preloadRef.current) {
+      preloadRef.current = Preload();
 
-    refGeneratorWorker.current = new AssetsGenerator("", "asset.worker.js");
+      preloadRef.current.fetch(videoLinks);
 
-    refGeneratorWorker.current
-      .loadWorker()
-      .then(({ readyState }) => {
-        setProgress(1);
-        setWorker(readyState);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+      preloadRef.current.oncomplete = (items) => {
+        setVideos(items);
+        setIsLoaded(true);
+      };
+
+      preloadRef.current.onprogress = (event) => {
+        progressRef.current.innerText = event.progress + "%";
+      };
+    }
   }, []);
 
-  // Load assets
-  useEffect(() => {
-    if (worker !== READY_STATES.INTERACTIVE || isLoading || isLoaded) return;
-
-    setIsLoading(true);
-
-    const throttleProgress = throttle((progress) => {
-      setProgress(progress);
-    }, THROTTLE_WAIT);
-
-    refGeneratorWorker.current
-      .createAssets(videoLinks, "", "", false, (progress) => {
-        throttleProgress(progress);
-      })
-      .then(({ message, assets }) => {
-        setIsLoaded(true);
-        setAssets(assets);
-      })
-      .catch((e) => {
-        console.error(e);
-        setWorker(e.readyState);
-      });
-  }, [worker]);
-  return <div className="App">{isLoaded ? <SliderVideos videos={assets} /> : <p>{Math.round(progress)}%</p>}</div>;
+  return (
+    <div className="App">
+      {isLoaded ? (
+        <SliderVideos videos={videos} />
+      ) : (
+        <Loader>
+          <p>chargement</p>
+          <p ref={progressRef}></p>
+        </Loader>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/SliderVideos.jsx
+++ src/components/SliderVideos.jsx
@@ -1,44 +1,23 @@
-import React, { useState, useEffect, useRef, useCallback, Suspense } from "react";
-import { suspend } from "suspend-react";
-import { PreloadMedia, MediaType } from "react-preload-media";
-import blobToDataURL from "@/utils/blobToDataUrl";
-import { gsap } from "gsap";
+import React, { useEffect, useRef } from "react";
+
 // Modules
 import Slider from "@/modules/Slider.js";
-
-// Components
-import Spinner from "./Spinner";
 
 // Styles
 import styled from "styled-components";
 
-// const media = [
-//   {
-//     type: MediaType.Video,
-//     url: "https://player.vimeo.com/progressive_redirect/playback/745429773/rendition/1080p/file.mp4?loc=external&signature=9a1f98f1c56ffd0b67690bb358d1c897a02390b0d2d2b8c841e80478cbc69273",
-//   },
-//   {
-//     type: MediaType.Video,
-//     url: "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
-//   },
-// ];
-
-const media = [
-  { type: MediaType.Image, url: "https://via.placeholder.com/150" },
-  { type: MediaType.Image, url: "https://via.placeholder.com/300" },
-  { type: MediaType.Audio, url: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3" },
+const videoLinks = [
+  "https://player.vimeo.com/progressive_redirect/playback/745429773/rendition/1080p/file.mp4?loc=external&signature=9a1f98f1c56ffd0b67690bb358d1c897a02390b0d2d2b8c841e80478cbc69273",
+  "https://player.vimeo.com/progressive_redirect/playback/745436813/rendition/1080p/file.mp4?loc=external&signature=21ef6b89911f7c316c93d6274fbc86602b7a62b4d0b46cf8481ea6825e0e0df5",
+  "https://player.vimeo.com/progressive_redirect/playback/745438360/rendition/1080p/file.mp4?loc=external&signature=d9cd1b7eb2e898e30e8abb2764d2e8b9d4ea1ce0f7e78101a5e2998e4d5de042",
+  "https://player.vimeo.com/progressive_redirect/playback/745439503/rendition/1080p/file.mp4?loc=external&signature=73b07aa2a65ce25f9f0533cd207d1ad5907489bf596ebe1d37e7fb2274434855",
+  "https://player.vimeo.com/progressive_redirect/playback/745435539/rendition/1080p/file.mp4?loc=external&signature=006c81c361a16764fb9bd8b6e2a23b6bab008e9dd563eef80e206db62a806f2b",
+  "https://player.vimeo.com/progressive_redirect/playback/755610395/rendition/1080p/file.mp4?loc=external&signature=9952f9d547936b19357794a86e1d4cacffeefc9665ca1665ac28ecfbe8df55ea",
 ];
-
-const videoLinks = ["./videos/1.mp4", "./videos/2.mp4", "./videos/3.mp4", "./videos/4.mp4"];
 
 const Container = styled.div`
   height: 100%;
   overflow: hidden;
-`;
-const Loader = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
 `;
 
 const VideoContainer = styled.div`
@@ -49,80 +28,33 @@ const VideoContainer = styled.div`
   height: 100vh;
   height: -webkit-fill-available;
 
-  & > div {
-    width: 100%;
-    height: 100%;
-  }
   video {
     width: 100%;
     height: 100%;
     object-fit: cover;
     transition: opacity 300ms;
-
-    &.is-loaded {
-      opacity: 1;
-    }
   }
 `;
 
-const Video = ({ src }) => {
-  // Refs
-  const containerRef = useRef();
-
-  return (
-    <>
-      <video src={src} crossOrigin="anonymous" muted react-preload-media loop playsInline autoPlay />
-    </>
-  );
-};
-
 const SliderVideos = ({ videos }) => {
-  // State
-  const [assetsUrlsCreated, setAssetsUrlsCreated] = useState(false);
   // Refs
   const sliderRef = useRef();
   const containerRef = useRef();
   const videoContainerRef = useRef();
 
-  useEffect(() => {});
-
-  // Create assets urls
-  const createAssetsUrls = useCallback(async (assets) => {
-    await Promise.all(
-      Object.values(assets).map((asset) => {
-        gsap.delayedCall(0.1, async () => {
-          asset.url = await blobToDataURL(asset.blob);
-        });
-      })
-    );
-
-    setAssetsUrlsCreated(true);
-  }, []);
-
-  useEffect(() => {
-    if (!assetsUrlsCreated) {
-      createAssetsUrls(videos);
-    }
-  }, []);
-
-  console.log(videos);
-
   // Init Slider
   useEffect(() => {
-    if (!sliderRef.current && assetsUrlsCreated) {
+    if (!sliderRef.current) {
       sliderRef.current = new Slider({ container: containerRef.current });
     }
-  }, [assetsUrlsCreated]);
-
-  if (!assetsUrlsCreated) return null;
+  }, []);
 
   return (
     <>
       <Container ref={containerRef}>
         {videos.map((video, i) => (
           <VideoContainer key={i} ref={videoContainerRef}>
-            <video src={video.url} crossOrigin="anonymous" muted loop playsInline autoPlay />
-            {/* <Video src={videoLink} onAssetLoad={onAssetLoad} /> */}
+            <video src={video.blobUrl} crossOrigin="anonymous" muted loop playsInline autoPlay />
           </VideoContainer>
         ))}
       </Container>
